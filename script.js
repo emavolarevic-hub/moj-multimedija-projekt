99f3be1651ca6f8bc502e090e745371e35042d45
emavolarevic-hub <ema.volarevic@ss-adamic.com>
2026-05-12 14:13:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
 function showSection(id) {
 
-    // sakrij početni meni
     document.querySelector(".menu").classList.add("hidden");
 
-    // sakrij sve sekcije
     document.querySelectorAll(".page").forEach(sec => {
         sec.classList.add("hidden");
     });
 
-    // prikaži odabranu sekciju
     document.getElementById(id).classList.remove("hidden");
 
-    // prikaži strelicu
     document.getElementById("backBtn").classList.remove("hidden");
 }
 
 function goHome() {
 
-    // sakrij sve sekcije
     document.querySelectorAll(".page").forEach(sec => {
         sec.classList.add("hidden");
     });
 
-    // prikaži početni meni
     document.querySelector(".menu").classList.remove("hidden");
 
-    // sakrij strelicu
     document.getElementById("backBtn").classList.add("hidden");
 }
+
 function openLightbox(src) {
     document.getElementById("lightbox-img").src = src;
     document.getElementById("lightbox").classList.remove("hidden");
