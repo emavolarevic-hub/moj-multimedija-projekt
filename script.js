36be7fee88578873007496a78e7993f643d09abc
emavolarevic-hub <ema.volarevic@ss-adamic.com>
2026-05-12 14:07:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -28,3 +28,11 @@ function goHome() {
     // sakrij strelicu
     document.getElementById("backBtn").classList.add("hidden");
 }
+function openLightbox(src) {
+    document.getElementById("lightbox-img").src = src;
+    document.getElementById("lightbox").classList.remove("hidden");
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").classList.add("hidden");
+}
